03972c8350f1c36c1d4c66ef388784f29a977a14
qianwl <123456>
2021-03-10 11:52:34 +0800

    02-2.3

--- src/main/java/com/itcodai/course02/configuration/FastJsonMyConfig.java
+++ src/main/java/com/itcodai/course02/configuration/FastJsonMyConfig.java
@@ -1,11 +1,15 @@
 package com.itcodai.course02.configuration;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,13 +22,34 @@ import java.util.List;
 public class FastJsonMyConfig extends WebMvcConfigurationSupport {
 
     /**
-     * 使用
+     * 使用阿里FastJson作为Json MessageConverter
+     *
      * @param converters
      */
     @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters){
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
         FastJsonConfig config = new FastJsonConfig();
-        config.setSerializerFeatures();
+        config.setSerializerFeatures(
+                //保留map空的字段
+                SerializerFeature.WriteMapNullValue,
+                //将String类型的null转成""
+                SerializerFeature.WriteNullStringAsEmpty,
+                //将Number类型的null转换成0
+                SerializerFeature.WriteNullNumberAsZero,
+                //将List类型的转成[]
+                SerializerFeature.WriteNullListAsEmpty,
+                //将Boolean类型的null转成null
+                SerializerFeature.WriteNullBooleanAsFalse,
+                //避免循环引用
+                SerializerFeature.DisableCircularReferenceDetect
+        );
+        converter.setFastJsonConfig(config);
+        converter.setDefaultCharset(Charset.forName("UTF-8"));
+        List<MediaType> mediaTypeList = new ArrayList<>();
+        //解决中午乱码问题，相当于再Controller上的@RequestMapping中加了个属性produces = "application/json"
+        mediaTypeList.add(MediaType.APPLICATION_JSON);
+        converter.setSupportedMediaTypes(mediaTypeList);
+        converters.add(converter);
     }
 }
